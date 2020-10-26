7c822239ab1748af31632b63517655afa071a031
Ali Rahimi <ali.hamburg@gmail.com>
2020-10-26 13:29:11 +0100

    adds singleton example

---
+++ TestProject/src/patterns/SingletonPattern.java
@@ -0,0 +1,24 @@
+package patterns;
+
+public class SingletonPattern {
+
+	public static void main(String[] args) {
+		Singleton singleInstance1 = Singleton.getInstance();
+		Singleton singleInstance2 = Singleton.getInstance();
+		
+		System.out.println(singleInstance1==singleInstance2);
+	}
+}
+
+class Singleton {
+	private static Singleton instance;
+	
+	public static Singleton getInstance() {
+		synchronized (instance) {
+			if(instance == null) {
+				instance = new Singleton();
+			}
+		}
+		return instance;
+	}
+}
