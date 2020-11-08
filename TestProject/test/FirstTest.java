8edf2cffc4783e07537f10754e9e4a35b0f8c5e1
Ali Rahimi <ali.hamburg@gmail.com>
2020-11-08 13:51:53 +0100

    adds first junit test

---
+++ TestProject/test/FirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+    @Test
+    public void testAdd(){
+        assertEquals(2,Integer.sum(1,1));
+    }
+
+    @Test
+    public void testSub(){
+        assertEquals(1,Integer.toString(1));
+    }
+}
